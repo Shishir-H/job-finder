ea0ef0559c58c99b20707d924ba07548cd098834
shreyashc <shreyashc018@gmail.com>
2020-12-30 16:11:41 +0530

    styles for student n company index

--- company/index.php
+++ company/index.php
@@ -3,6 +3,8 @@
     session_start();
     require_once '../db/connect.php';
     $id = $_SESSION['id'];
+    $username = $_SESSION['username'];
+    $email = $_SESSION['email'];
     $query = "SELECT * FROM `application` WHERE company_id='$id';";
     $result = mysqli_query($conn, $query);
 
@@ -22,44 +24,54 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,300;1,400;1,700&display=swap"
-            rel="stylesheet"
-        />
-        <link rel="stylesheet" href="../styles/style.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,300;1,400;1,700&display=swap"
+            rel="stylesheet" />
 
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../styles/style.css" />
+        <link rel="stylesheet" href="../styles/home.css" />
 
         <title>Home</title>
     </head>
 
     <body>
         <nav class="nav-bar">
-            <h1>Job Finder</h1>
-            <a class="nav-btn" href="profile.php">My profile</a>
+            <h1>Job Finder <i class="fa fa-suitcase" aria-hidden="true"></i></h1>
+            <a class="nav-btn" href="../index.html">Logout</a>
         </nav>
-        <h2>My Jobs Listings</h2>
-        <div>
-            <a href="listajob.php">List a new Job</a>
-        </div>
-        <div class="card">
-            <div class="jobs">
-                <!-- dummy - remove this later -->
-                <div class="job">
-                    <div class="title">This is Job Title</div>
-                    <div class="short-desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit harum,
-                        eveniet quos enim cum autem nobis dolor corrupti mollitia. Culpa?</div>
-                </div>
+        <h1>MY JOB LISTINGS</h1>
+
+
+        <div class="wrapper">
+            <div class="notification">
+                <div class="not">Notifications <i class="fa fa-bell" aria-hidden="true"></i></div>
+                <div class="line"></div>
+            </div>
+            <div class="job-list-card">
                 <?php 
                 while($row = mysqli_fetch_assoc($result)){ ?>
-                <div class="job">
-                    <div class="title">
-                        <a href="application.php?aid=<?php echo($row["id"])?>"> <?php echo($row["job_title"]) ?> </a>
+                <div class="job-list-card__job">
+                    <div>
+                        <a class="job-list-card__job-title" href="application.php?aid=<?php echo($row["id"])?>">
+                            <?php echo($row["job_title"]) ?> </a>
                     </div>
-                    <div class="short-desc"><?php echo($row["job_description"])?></div>
+                    <div class="job-list-card__short-desc"><?php echo($row["job_description"])?></div>
                 </div>
+                <?php } ?>
             </div>
-            <?php } ?>
-        </div>
+            <div class="user-info">
+                <div class="wrap1">
+                    <div class="usr-icon"><i class="fa fa-user-circle-o" aria-hidden="true"></i></div>
+                    <div class="wrap__inner">
+                        <div class="user-info__username"><?php echo($username)?></div>
+                        <div class="user-info__email"><?php echo($email)?></div>
+                    </div>
+                </div>
+                <a class="prof-btn" href="profile.php">My profile</a>
+                <a href="listajob.php" class="list-btn">List a new Job</a>
+
+            </div>
         </div>
     </body>
 
